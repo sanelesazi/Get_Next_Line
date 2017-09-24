6f10cb6b8edc3c49980ecc530ef6695e328cc24e
Sanele SIBIYA <ssibiya@e5r7p6.wethinkcode.co.za>
2017-09-24 12:03:08 +0200

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssibiya <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/18 11:20:41 by ssibiya           #+#    #+#             */
+/*   Updated: 2017/09/24 12:02:20 by ssibiya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _GET_NEXT_LINE_H
+# define _GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# define BUFF_SIZE 32
+
+int	get_next_line(int fd, char **line);
+
+#endif
